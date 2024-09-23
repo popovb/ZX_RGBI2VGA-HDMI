7c308ad7d7413fc44dd9f5e634cb3764cde4969c
Boris Popov <popov@whitekefir.ru>
2024-09-23 15:46:17 +0300

    begin Configuration

---
+++ ZX_RGB_TO_VGA_HDMI/Configuration.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "Pins.hpp"
+
+#ifndef _ZXRGB_CONFIGURATION_HPP_
+#define _ZXRGB_CONFIGURATION_HPP_
+
+namespace zxrgb {
+
+     class Configuration {
+
+     public:
+	  static Pins pins;
+     };
+}
+#endif // _ZXRGB_CONFIGURATION_HPP_
