3df6ba5ffb9d1be28bbee2cfac074d18bc900253
Boris Popov <popov@whitekefir.ru>
2024-10-31 15:02:49 +0300

    add proto for SyncMode

---
+++ zxrgb_2_vga_hdmi/SyncMode.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
+//
+
+#ifndef _ZXRGB_SYNC_MODE_HPP_
+#define _ZXRGB_SYNC_MODE_HPP_
+
+namespace zxrgb {
+
+     class SyncMode {
+
+     public:
+	  enum mode_t {
+	       S,
+	       C,
+	  };
+     };
+}
+#endif // _ZXRGB_SYNC_MODE_HPP_
