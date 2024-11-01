df13b8d1e2a16f7f683b6764b9fb984e193f09f3
Boris Popov <popov@whitekefir.ru>
2024-11-01 16:52:24 +0300

    add impl for SettingsLoader::load

--- zxrgb_2_vga_hdmi/SettingsLoader.cpp
+++ zxrgb_2_vga_hdmi/SettingsLoader.cpp
@@ -3,22 +3,54 @@
 //
 
 #include "SettingsLoader.hpp"
+#include <Arduino.h>
 
 zxrgb::SettingsLoader::SettingsLoader(CaptureSettings& v):
      cs(v)
 {
      return;
 }
-/*
-bool zxrgb::SettingsLoader::load(CaptureSettings& cs, char* s) const {
-     //if (
-     //
-     // TODO
-     //
-     return true;
+
+void zxrgb::SettingsLoader::load(const char* s, int v) const {
+     if (shift_x(s, v)) return;
+     if (shift_y(s, v)) return;
+     if (clk_mode(s, v)) return;
+     if (video_mode(s, v)) return;
+     if (sync_mode(s, v)) return;
+     if (x3_buff(s, v)) return;
+     if (int_freq(s, v)) return;
+     if (ext_freq_div(s, v)) return;
+     if (delay(s, v)) return;
+     if (delay_rise(s, v)) return;
+     if (delay_fall(s, v)) return;
+     if (inv_pin_mask(s, v)) return;
+     if (len_vs(s, v)) return;
+     if (color_mode(s, v)) return;
+     if (wide(s, v)) return;
+     Serial.printf("wrong command\n");
 }
 
-
+/*
+  if (strcmp(s_key+1, "cap_sh_x")==0) CAP_SET_LOAD(capture_setings.shX,int)
+  if (strcmp(s_key+1, "cap_sh_y")==0) CAP_SET_LOAD(capture_setings.shY,int)
+  if (strcmp(s_key+1, "cap_delay")==0) CAP_SET_LOAD(capture_setings.capture_delay,int)
+  if (strcmp(s_key+1, "cap_delay_fall")==0) CAP_SET_LOAD(capture_setings.capture_delay_fall,int)
+  if (strcmp(s_key+1, "cap_delay_rise")==0) CAP_SET_LOAD(capture_setings.capture_delay_rise,int)
+  if (strcmp(s_key+1, "cap_ext_f_div")==0) CAP_SET_LOAD(capture_setings.ext_freq_div,int)
+  if (strcmp(s_key+1, "cap_sync_mode")==0) CAP_SET_LOAD(capture_setings.in_sync_mode,in_sync_mode_t)
+  if (strcmp(s_key+1, "cap_len_VS")==0) CAP_SET_LOAD(capture_setings.len_VS,int)
+  if (strcmp(s_key+1, "cap_p_clk_mode")==0) CAP_SET_LOAD(capture_setings.p_clk_mode,p_clk_mode_t)
+  if (strcmp(s_key+1, "video_out")==0) CAP_SET_LOAD(capture_setings.video_out_mode,video_out_mode_t)
+  if (strcmp(s_key+1, "cap_int_f")==0) CAP_SET_LOAD(capture_setings.int_freq,int)
+  if (strcmp(s_key+1, "cap_in_inv_mask")==0) CAP_SET_LOAD(capture_setings.inv_capture_pin_mask,int)
+  if (strcmp(s_key+1, "is_3X_bufmode")==0) CAP_SET_LOAD(capture_setings.is_3X_Buf,int)
+  if (strcmp(s_key+1, "c_mode")==0) CAP_SET_LOAD(capture_setings.c_mode,c_mode_t)
+  if (strcmp(s_key+1, "wide_mode")==0) CAP_SET_LOAD(capture_setings.is_wide_mode,int)
+  
+printf("wrong command\n");
+}
+*/
+/*
 void CAP_SET_LOAD(x, T) {
      if(s_key[0]=='r' || s_key[0]=='w') {
 	  if(s_key[0]=='w') (x)=static_cast<T>(s_data);
@@ -27,29 +59,5 @@ void CAP_SET_LOAD(x, T) {
 	  continue;
      }
 }
-*/
-/*
-//обработчик загрузки параметров захвата
 #define CAP_SET_LOAD(x,T) {if(s_key[0]=='r' || s_key[0]=='w') {  if(s_key[0]=='w') (x)=static_cast<T>(s_data); check_cap_data(&capture_setings); printf("%s %d\n",s_key,(x)); continue;};};
-	       
-//смещение изображения
-if (strcmp(s_key+1, "cap_sh_x") == 0)        CAP_SET_LOAD(capture_setings.shX, int)
-if (strcmp(s_key+1, "cap_sh_y") == 0)        CAP_SET_LOAD(capture_setings.shY, int)
-if (strcmp(s_key+1, "cap_delay") == 0)       CAP_SET_LOAD(capture_setings.capture_delay, int)
-if (strcmp(s_key+1, "cap_delay_fall") == 0)  CAP_SET_LOAD(capture_setings.capture_delay_fall, int)
-if (strcmp(s_key+1, "cap_delay_rise") == 0)  CAP_SET_LOAD(capture_setings.capture_delay_rise, int)
-if (strcmp(s_key+1, "cap_ext_f_div") == 0)   CAP_SET_LOAD(capture_setings.ext_freq_div, int)
-if (strcmp(s_key+1, "cap_sync_mode") == 0)   CAP_SET_LOAD(capture_setings.in_sync_mode, in_sync_mode_t)
-if (strcmp(s_key+1, "cap_len_VS") == 0)      CAP_SET_LOAD(capture_setings.len_VS,int)
-if (strcmp(s_key+1, "cap_p_clk_mode") == 0)  CAP_SET_LOAD(capture_setings.p_clk_mode, p_clk_mode_t)
-if (strcmp(s_key+1, "video_out") == 0)       CAP_SET_LOAD(capture_setings.video_out_mode, video_out_mode_t)
-if (strcmp(s_key+1, "cap_int_f") == 0)       CAP_SET_LOAD(capture_setings.int_freq, int)
-if (strcmp(s_key+1, "cap_in_inv_mask") == 0) CAP_SET_LOAD(capture_setings.inv_capture_pin_mask, int)
-if (strcmp(s_key+1, "is_3X_bufmode") == 0)   CAP_SET_LOAD(capture_setings.is_3X_Buf, int)
-if (strcmp(s_key+1, "c_mode") == 0)          CAP_SET_LOAD(capture_setings.c_mode, c_mode_t)
-if (strcmp(s_key+1, "wide_mode") == 0)       CAP_SET_LOAD(capture_setings.is_wide_mode, int)
-
-printf("wrong command\n");
-
-}
 */
--- zxrgb_2_vga_hdmi/SettingsLoader.hpp
+++ zxrgb_2_vga_hdmi/SettingsLoader.hpp
@@ -18,6 +18,23 @@ namespace zxrgb {
 
      private:
 	  CaptureSettings& cs;
+
+     private:
+	  bool shift_x(const char*, int) const;
+	  bool shift_y(const char*, int) const;
+	  bool clk_mode(const char*, int) const;
+	  bool video_mode(const char*, int) const;
+	  bool sync_mode(const char*, int) const;
+	  bool x3_buff(const char*, int) const;
+	  bool int_freq(const char*, int) const;
+	  bool ext_freq_div(const char*, int) const;
+	  bool delay(const char*, int) const;
+	  bool delay_rise(const char*, int) const;
+	  bool delay_fall(const char*, int) const;
+	  bool inv_pin_mask(const char*, int) const;
+	  bool len_vs(const char*, int) const;
+	  bool color_mode(const char*, int) const;
+	  bool wide(const char*, int) const;
      };
 }
 #endif // _ZXRGB_SETTINGS_LOADER_HPP_
