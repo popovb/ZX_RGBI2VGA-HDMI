554adf9d25f963baccec8288bce0ff54d0973896
Boris Popov <popov@whitekefir.ru>
2024-11-02 10:20:48 +0300

    add impl for SettingsLoader::video_mode

--- zxrgb_2_vga_hdmi/SettingsLoader.cpp
+++ zxrgb_2_vga_hdmi/SettingsLoader.cpp
@@ -50,6 +50,13 @@ bool zxrgb::SettingsLoader::clk_mode(const char* s, int v) const {
      return true;
 }
 
+bool zxrgb::SettingsLoader::video_mode(const char* s, int v) const {
+     if (! check(s + 1, "video_out") ) return false;
+     if (! check(s[0]) ) return false;
+     set(cs.video_mode, s, v);
+     return true;
+}
+
 bool zxrgb::SettingsLoader::check(char v) const {
      if (v == 'w') return true;
      if (v == 'r') return true;
@@ -67,7 +74,6 @@ bool zxrgb::SettingsLoader::check(const char* a, const char* b) const {
   if (strcmp(s_key+1, "cap_ext_f_div")==0) CAP_SET_LOAD(capture_setings.ext_freq_div,int)
   if (strcmp(s_key+1, "cap_sync_mode")==0) CAP_SET_LOAD(capture_setings.in_sync_mode,in_sync_mode_t)
   if (strcmp(s_key+1, "cap_len_VS")==0) CAP_SET_LOAD(capture_setings.len_VS,int)
-  if (strcmp(s_key+1, "video_out")==0) CAP_SET_LOAD(capture_setings.video_out_mode,video_out_mode_t)
   if (strcmp(s_key+1, "cap_int_f")==0) CAP_SET_LOAD(capture_setings.int_freq,int)
   if (strcmp(s_key+1, "cap_in_inv_mask")==0) CAP_SET_LOAD(capture_setings.inv_capture_pin_mask,int)
   if (strcmp(s_key+1, "is_3X_bufmode")==0) CAP_SET_LOAD(capture_setings.is_3X_Buf,int)
