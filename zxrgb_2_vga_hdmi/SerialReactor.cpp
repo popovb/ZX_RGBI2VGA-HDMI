74eac6d794bd6accfba6cfbb80ea78855a4f12ea
Boris Popov <popov@whitekefir.ru>
2024-11-07 17:04:42 +0300

    add impl for SerialReactor::print_reset

--- zxrgb_2_vga_hdmi/SerialReactor.cpp
+++ zxrgb_2_vga_hdmi/SerialReactor.cpp
@@ -55,7 +55,6 @@ void zxrgb::SerialReactor::handle1() const {
 }
      /*
      if (strcmp(s_key, "reset")==0 || strcmp(s_key, "restart")==0 ) {printf("reset...\n");rp2040.restart();};
-     if (strcmp(s_key, "mode")==0) { printf("mode 1\n"); return;};
      if (strcmp(s_key+1, "cap_sh_x")==0) {if(s_key[0]=='w') set_cap_shx(s_data); return;}
      if (strcmp(s_key+1, "cap_sh_y")==0) {if(s_key[0]=='w') set_cap_shy(s_data); return;}
      */
@@ -67,10 +66,13 @@ bool zxrgb::SerialReactor::handle_ping() const {
 
 bool zxrgb::SerialReactor::handle_reset() const {
      if (! check("reset") ) return false;
-     // Serial.printf("ping ok\n");
      print_reset();
      restart();
      return true;
+}
+
+void zxrgb::SerialReactor::print_reset() const {
+     Serial.printf("reset...\n");
 }
 
 bool zxrgb::SerialReactor::handle_exit() const {
