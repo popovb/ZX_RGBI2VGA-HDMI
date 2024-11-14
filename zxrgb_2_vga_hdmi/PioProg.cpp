921dcf3c369eaa1819536329ef932b671e31f929
Boris Popov <popov@whitekefir.ru>
2024-11-14 17:26:34 +0300

    add impl for dtor for PioProg

---
+++ zxrgb_2_vga_hdmi/PioProg.cpp
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "PioProg.hpp"
+
+zxrgb::PioProg::~PioProg() {
+     return;
+}
--- zxrgb_2_vga_hdmi/PioProg.hpp
+++ zxrgb_2_vga_hdmi/PioProg.hpp
@@ -2,6 +2,8 @@
 // Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
 //
 
+#include "Types.hpp"
+
 #include "hardware/pio.h"
 
 #ifndef _ZXRGB_PIO_PROG_HPP_
@@ -12,10 +14,13 @@ namespace zxrgb {
      class PioProg {
 
      public:
-	  PioProg();
 	  virtual ~PioProg() = 0;
 
 	  const pio_program* get_pio_program() const;
+
+     protected:
+	  static const u8 len = 17;
+	  u16 instructions[len];
 
      private:
      };
