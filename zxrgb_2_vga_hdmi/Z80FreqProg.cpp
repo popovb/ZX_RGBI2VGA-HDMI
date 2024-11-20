845ad60d0aef28ff3cf8bcf254ea6a9e7810f9d4
Boris Popov <popov@whitekefir.ru>
2024-11-20 10:26:31 +0300

    add impl for Z80FreqProg::cmds

--- zxrgb_2_vga_hdmi/Z80FreqProg.cpp
+++ zxrgb_2_vga_hdmi/Z80FreqProg.cpp
@@ -6,28 +6,20 @@
 #include "CapturedPins.hpp"
 
 static zxrgb::u16 F = zxrgb::CapturedPins::f;
-/*
+
 zxrgb::u16
 zxrgb::Z80FreqProg::cmds[] =
 {
-     0xa042,            //  0: nop
-     0xe020,            //  1: set x, 0
-     (u16)(0x2000 | F), //  2: wait 1 gpio, 0
-     0x4008,            //  3: in pins, 8
-     (u16)(0x2080 | F), //  4: wait 0 gpio, 0
-     0x0042,            //  5: jmp x--, 2
-     0x8020,            //  6: push block
-     0x00c1,            //  7: jmp pin, 1
-     0xe020,            //  8: set x, 0
-     (u16)(0x2000 | F), //  9: wait 1 gpio, 0
-     0x00c0,            // 10: jmp pin, 0
-     0x4008,            // 11: in pins, 8
-     (u16)(0x2080 | F), // 12: wait 0 gpio, 1
-     0x0049,            // 13: jmp x--, 9
-     0x8020,            // 14: push block
-     0x0008,            // 15: jmp 8
+     (u16)(0x2080 | F), //  0: wait 1 gpio, 0
+     0xa042,            //  1: nop
+     0x4008,            //  2: in pins, 8
+     0x8020,            //  3: push block
+     (u16)(0x2000 | F), //  4: wait 0 gpio, 0
+     0xa042,            //  5: nop
+     0x4008,            //  6: in pins, 8
+     0x8020,            //  7: push block
 };
-*/
+
 zxrgb::Z80FreqProg::Z80FreqProg() {
      prog.instructions = cmds;
      prog.length = 8;
