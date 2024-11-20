51ecf36e84d9710cd05d85e097eed734c3a76793
Boris Popov <popov@whitekefir.ru>
2024-11-20 10:13:48 +0300

    add impl for ctor for Z80FreqProg

---
+++ zxrgb_2_vga_hdmi/Z80FreqProg.cpp
@@ -0,0 +1,46 @@
+//
+// Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "Z80FreqProg.hpp"
+#include "CapturedPins.hpp"
+
+static zxrgb::u16 F = zxrgb::CapturedPins::f;
+/*
+zxrgb::u16
+zxrgb::Z80FreqProg::cmds[] =
+{
+     0xa042,            //  0: nop
+     0xe020,            //  1: set x, 0
+     (u16)(0x2000 | F), //  2: wait 1 gpio, 0
+     0x4008,            //  3: in pins, 8
+     (u16)(0x2080 | F), //  4: wait 0 gpio, 0
+     0x0042,            //  5: jmp x--, 2
+     0x8020,            //  6: push block
+     0x00c1,            //  7: jmp pin, 1
+     0xe020,            //  8: set x, 0
+     (u16)(0x2000 | F), //  9: wait 1 gpio, 0
+     0x00c0,            // 10: jmp pin, 0
+     0x4008,            // 11: in pins, 8
+     (u16)(0x2080 | F), // 12: wait 0 gpio, 1
+     0x0049,            // 13: jmp x--, 9
+     0x8020,            // 14: push block
+     0x0008,            // 15: jmp 8
+};
+*/
+zxrgb::Z80FreqProg::Z80FreqProg() {
+     prog.instructions = cmds;
+     prog.length = 8;
+     prog.origin = -1;
+}
+/*
+void zxrgb::Z80FreqProg::set_delay(u32 v) {
+     cmds[0] |= ((v & 0b11111) << 8);
+}
+
+void zxrgb::Z80FreqProg::set_ext_freq_div(u32 v) {
+     u32 vv = (v - 1) & 0b11111;
+     cmds[1] |= vv;
+     cmds[8] |= vv;
+}
+*/
